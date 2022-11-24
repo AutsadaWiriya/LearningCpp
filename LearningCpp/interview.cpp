afaf333920d3c9d3e6248d8293fa8b9ba8c99bc0
Autsada Wiriya <aut0935261784@gmail.com>
2022-11-24 12:07:48 +0700

    Add

---
+++ LearningCpp/interview.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   int age;
+   string gender;
+   cout << "Input your gender = " ;
+   cin >> gender;
+   cout << "Input your age = " ;
+   cin >> age;
+
+   cout << "-----Description-----" << endl;
+   cout << "Your gender = " << gender << endl;
+   cout << "Your age = " << age << endl;
+
+   bool condition1 = gender == "male" || gender == "female" ;
+   bool condition2 = age >= 25 ;
+
+   cout << "==========================================" << endl;
+   cout << "Status " << endl;
+   if(condition1 && condition2){
+      cout << "Pass Interviwe" << endl;
+   }
+   else{
+      cout << "Not pass Interview" << endl;
+   }
+}
